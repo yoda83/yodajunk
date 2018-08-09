21ac0f66f0b5be74b2491e32e61d382f952a1eb4
yoda83 <bjones1831@gmail.com>
2018-08-09 13:54:18 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 # This is a single line comment.
 
 resource "aws_instance" "baseNEW" {
-  ami           = "ami-db710fa3"
+  ami           = "ami-ba602bc2"
   instance_type = "t2.micro"
 
   tags {
-    Name = "terraform-jones-github-1"
+    Name = "terraform-jones-github-2"
     Application = "Infrastructure"
     Customer = "None"
     Environment = "ProofOfConcept"
